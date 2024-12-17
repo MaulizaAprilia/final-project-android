f62580e2d59a894853aef9d6e5be89bd13842358
MAULIZA APRILIA <118743543+MaulizaAprilia@users.noreply.github.com>
2024-12-17 21:20:51 +0700

    Final Project Android

---
+++ app/src/main/java/com/mauliza/finalproject/Activity/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.mauliza.finalproject.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.mauliza.finalproject.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
